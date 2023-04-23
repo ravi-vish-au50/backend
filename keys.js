bb0dca5d5cb97a9159dcb3f96f7d4a3db409c8b2
ravi-vish-au50 <110217289+ravi-vish-au50@users.noreply.github.com>
2023-04-24 01:18:22 +0530

    Add files via upload

---
+++ keys.js
@@ -0,0 +1,11 @@
+// module.exports = {
+//     mongoUrl: "mongodb+srv://ravivish166:ravi123@cluster0.zbfdjtv.mongodb.net/Instagram-clone",
+
+//     Jwt_secret: "faslkfocvneofu"
+// };
+
+module.exports = {
+    mongoUrl: "mongodb+srv://ravivish166:ravi123@cluster0.zbfdjtv.mongodb.net/Instagram-clone",
+    Jwt_secret: "faslkfocvneofu"
+
+};
